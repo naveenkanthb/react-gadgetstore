5c85da0d73f0df2d321bc4beba0a7498ce48438c
naveenkanthb <naveenkanth033@gmail.com>
2020-04-11 14:56:37 +0530

    updating product structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Contact from './pages/ContactPage';
+import Cart from './pages/CartPage';
+import Default from './pages/DefaultPage';
+import SingleProduct from './pages/SingleProductPage';
+import Products from './pages/ProductsPage';
+
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Sidecart from './components/Sidecart';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Sidebar />
+      <Sidecart />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id"  component={SingleProduct} />
+        <Route component={Default} />
+      </Switch>
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ src/pages/SingleProductPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SingleProductPage() {
+    return (
+        <div>
+            Hello from SingleProductPage
+        </div>
+    )
+}
